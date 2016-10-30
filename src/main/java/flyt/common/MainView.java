8f38ca9857d4515d769a224434e951655e1a358c
Anton Lindholm <anton.c.e.lindholm@gmail.com>
2016-10-30 17:39:40 +0200

    Added ValueChanged listener to CustomerLIist

--- src/main/java/flyt/common/MainView.java
+++ src/main/java/flyt/common/MainView.java
@@ -1,6 +1,8 @@
 package flyt.common;
 
+import com.vaadin.data.Property;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.VerticalLayout;
 import flyt.backend.Backend;
@@ -12,28 +14,35 @@ import java.util.List;
  */
 public class MainView extends MainDesign {
 
-    public MainView(){
+    public MainView() {
         super();
         clearContent();
         try {
             List<Customer> customerList = Backend.getInstance().getCustomers();
-            customerListSelect.setNullSelectionAllowed( false );
+            customerListSelect.setNullSelectionAllowed(false);
             for (Customer C : customerList) {
                 customerListSelect.addItem(C.name);
             }
             MenuBar.MenuItem menuItem = serverBar.getItems().get(0);
-            for ( Customer c : customerList ) {
-                for ( String server : c.servers ) {
-                    menuItem.addItem(server, (MenuBar.Command) mi -> System.out.println( server ));
+            for (Customer c : customerList) {
+                for (String server : c.servers) {
+                    menuItem.addItem(server, (MenuBar.Command) mi -> System.out.println(server));
                 }
             }
+            customerListSelect.addValueChangeListener(new Property.ValueChangeListener() {
+                @Override
+                public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+                    System.out.println("Display Customer stats");
+                }
+            });
             logoutButton.addClickListener(e -> {
                 System.out.println("User Logged Out");
             });
-        } catch ( FlytException fe ) {
-                // fix better error handling
+        } catch (FlytException fe) {
+            // fix better error handling
         }
     }
+
     public VerticalLayout getContent(){
 
         return content;
@@ -41,7 +50,7 @@ public class MainView extends MainDesign {
     }
     private void clearContent(){
             Label l = new Label();
-            l.setValue("Please select a Server or Check out the Statistics! :)");
+            l.setValue("Please select a Customer, a Server or Check out the Statistics! :)");
             content.removeAllComponents();
             content.addComponent(l);
         }
