8e00283b0ab7a1d5eac26c88e8904728387fab42
Fredrik <fredrik.byggmastar@gmail.com>
2016-11-06 19:50:12 +0200

    Added graphs

--- src/main/java/flyt/v2/ServerView.java
+++ src/main/java/flyt/v2/ServerView.java
@@ -1,14 +1,58 @@
 package flyt.v2;
 
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.*;
+import flyt.adapter.LatestServerStatePieChartData;
+import flyt.adapter.ServerLineChartData;
+import flyt.backend.Backend;
+import flyt.common.FlytException;
+import flyt.common.LineGraphComponent;
+import flyt.common.PieChartComponent;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Fredrik
  */
 public class ServerView extends CustomComponent {
     public ServerView( String serverId ) {
-        Label label = new Label( serverId );
-        setCompositionRoot( label );
+        try {
+            VerticalLayout vl = new VerticalLayout();
+            LatestServerStatePieChartData latest = new LatestServerStatePieChartData(Backend.getInstance(), serverId);
+            ServerLineChartData latestline = new ServerLineChartData(Backend.getInstance(), serverId, "Date", "Servers");
+
+            Label pieChart = new Label();
+            pieChart.setId("latest-pie");
+            Label lineChart = new Label();
+            lineChart.setId("server-line");
+
+            PieChartComponent PCS = new PieChartComponent();
+            PCS.setPieData(latest.getDataAsJson());
+            PCS.setPieOptions(latest.getTitle(),"latest-pie");
+            PCS.run();
+
+            LineGraphComponent LGC = new LineGraphComponent();
+            LGC.setLineData(latestline.getDataAsJson());
+            List<String> lineNames = latest.getLineNames();
+            String names = "";
+            for ( int i = 0; i < lineNames.size(); i ++ ) {
+                names += "'" + lineNames.get( i ) + "'";
+                if ( i != lineNames.size() - 1 ) {
+                    names += ",";
+                }
+            }
+            LGC.setOptions(names,"server-line", latestline.getHorizontalAxisName(), latestline.getVerticalAxisName());
+
+            LGC.run();
+
+            vl.addComponent(lineChart);
+            vl.addComponent(pieChart);
+            setCompositionRoot( vl );
+        }
+        catch (FlytException fe){
+            Notification.show( fe.getMessage(), Notification.Type.ERROR_MESSAGE );
+        }
+
+
     }
 }
